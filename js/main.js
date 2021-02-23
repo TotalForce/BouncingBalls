4a9e1a55298d77a16e5ca3a8de3b8050fe50750d
TotalForce <Total-Force@yandex.ru>
2021-02-23 16:11:27 +0500

    Better ui

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
  var canvas = document.querySelector('canvas');
  var ctx = canvas.getContext('2d');
 
- var width = canvas.width = window.innerWidth*0.8;
+ var width = canvas.width = window.innerWidth;
  var height = canvas.height = window.innerHeight*0.8;
-
- // function to generate random number
 
  function random(min,max) {
    var num = Math.floor(Math.random()*(max-min)) + min;
